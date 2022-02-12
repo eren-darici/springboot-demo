4cce6ad82a33f4b654e0bcca6e9c845ad0cd3636
Eren Darıcı <ed.erendarici@gmail.com>
2022-02-12 14:09:32 +0300

    Add update and delete routes

--- src/main/java/com/erendarici/springbootdemo/api/PersonController.java
+++ src/main/java/com/erendarici/springbootdemo/api/PersonController.java
@@ -5,6 +5,8 @@ import com.erendarici.springbootdemo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
@@ -33,5 +35,16 @@ public class PersonController {
     public Person getPersonById(@PathVariable("id") UUID id) {
         return personService.getPersonById(id)
                 .orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deletePersonById(@PathVariable("id") UUID id) {
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updatePersonById(@PathVariable("id") UUID id, @Valid @NotNull @RequestBody Person personToUpdate) {
+        personService.updatePerson(id, personToUpdate);
+
     }
 }
